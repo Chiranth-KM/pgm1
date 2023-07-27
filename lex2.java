a0620e7fbab1aa194c0da6f96b19b0013c4b4e76
Chiranth KM <121375834+Chiranth-KM@users.noreply.github.com>
2023-07-27 07:30:37 +0530

    Add files via upload

---
+++ lex2.java
@@ -0,0 +1,19 @@
+package LABpack;
+
+import java.util.*;
+public class lex2 {
+    public static void main(String[] args) {
+        System.out.println("Enter five names: ");
+        String array[] = new String[5];       // Declaration of string array
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            array[i] = sc.nextLine();
+        }
+        Arrays.sort(array);
+        System.out.print("Sorted list is: ");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
+
